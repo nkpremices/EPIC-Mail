6aaa4253bd2830cab7b91ad2e8ced1bf408a34eb
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-07 21:27:29 +0200

    [Feature #164441516] Arrange the code of the manage.home.js file according to eslint conventions

--- UI/js/manage.home.js
+++ UI/js/manage.home.js
@@ -1,30 +1,35 @@
 //Going to sign in page from the nav
-var callSignin = function ()  {
+// eslint-disable-next-line 
+var callSignin = function ()  { // eslint-disable-line no-use-before-define
 
     //changing the display of the navigation 
-     document.getElementById('home-nav').className="li nav-inactive";
-     document.getElementById('signin-nav').className="li nav-active";
-     document.getElementById('signup-nav').className="li nav-inactive";
-     window.location='html/signupIn.html';
-     signIn;
+    document.getElementById('home-nav').className='li nav-inactive';
+    document.getElementById('signin-nav').className='li nav-active';
+    document.getElementById('signup-nav').className='li nav-inactive';
+    window.location='html/signupIn.html';
+    // eslint-disable-next-line 
+     signIn();
 
 };
 
 //Going to sign up page from the nav
-var callSignup = function ()  {
+// eslint-disable-next-line 
+var callSignup = function ()  { // eslint-disable-line no-use-before-define
     //changing the display of the navigation 
-    document.getElementById('home-nav').className="li nav-inactive";
-    document.getElementById('signin-nav').className="li nav-inactive";
-    document.getElementById('signup-nav').className="li nav-active";
+    document.getElementById('home-nav').className='li nav-inactive';
+    document.getElementById('signin-nav').className='li nav-inactive';
+    document.getElementById('signup-nav').className='li nav-active';
     window.location='html/signupIn.html';
-    signUp;
+    // eslint-disable-next-line 
+    signUp();
 };
 
 //Going to Home page from the nav
-var callHome = function ()  {
+// eslint-disable-next-line 
+var callHome = function ()  { // eslint-disable-line no-use-before-define
     //changing the display of the navigation 
-    document.getElementById('home-nav').className="li nav-active";
-    document.getElementById('signin-nav').className="li nav-inactive";
-    document.getElementById('signup-nav').className="li nav-inactive";
+    document.getElementById('home-nav').className='li nav-active';
+    document.getElementById('signin-nav').className='li nav-inactive';
+    document.getElementById('signup-nav').className='li nav-inactive';
 };
 
