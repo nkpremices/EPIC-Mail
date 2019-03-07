d6047e5933af105174893fde882d5381c85580e0
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-07 21:34:01 +0200

    [Feature #164441516] clean the index.js file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,1 @@
-let someStuf = false;
-
-const showMessage = ()=>{
-    for (let index = 0; index < array.length; index++) {
-        console.console.log(`the stuff is made`);
-        
-        
-    }
-};
+ 
