a07e9f7f82a658d70887f71ea9470db6d2a08cdd
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-07 17:06:29 +0200

    [Feature #164441516] Add a template for eslint test

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+let someStuf = false;
+
+const showMessage = ()=>{
+    for (let index = 0; index < array.length; index++) {
+        console.console.log(`the stuff is made`);
+        
+        
+    }
+};
