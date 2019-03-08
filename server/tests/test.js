1a7f229b704eabc93781a76eeaaa04561765573c
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-08 22:32:13 +0200

    [feature #164441516] Add the test.js file to the project

---
+++ server/tests/test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../routes/index');// eslint-disable-line
+
+chai.use(chaiHttp);
+
+const should = chai.should();// eslint-disable-line
+
+describe('Todo', () => {// eslint-disable-line
+    it('should return an array', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .get('/')
+            .end((err, res) => {
+                res.body.should.be.an('Object');
+                done();
+            });
+    });
+
+    it('should have property todo', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .get('/')
+            .end((err, res) => {  // eslint-disable-line           
+                res.body.should.have.property('todo');
+                done();
+            });
+    });
+});
