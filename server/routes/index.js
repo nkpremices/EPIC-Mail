ae4197b849f3dce1368b2021d2894b6d2fc69500
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-08 22:22:52 +0200

    [feature #164441516] Add the index.js file to the project

---
+++ server/routes/index.js
@@ -0,0 +1,39 @@
+import { express } from 'express';
+import { bodyParser } from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extend: true }));
+app.use(bodyParser.json());
+
+const todo = [
+    { id: 1, description: 'Clean the house', createdOn: '08/02/2019' },
+    { id: 2, description: 'wash the deshes', createdOn: '08/03/2019' },
+    { id: 3, description: 'Complete my Assignement', createdOn: '08/04/2019' },
+    { id: 4, description: 'Write some code', createdOn: '08/05/2019' },
+];
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    const { id } = req.body;
+    const descp = req.body.description;
+    const creation = Date.now();// eslint-disable-line
+    const exist = todo.find(item => {// eslint-disable-line
+        return item.id === parseInt(id, 10);
+    });
+    res.send({
+        id, descp,
+    });
+});
+
+app.get('/', (req, res) => {
+    res.send({
+        todo, statusCode: 200,
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server running on 3000');
+});
+
+module.exports = app;
