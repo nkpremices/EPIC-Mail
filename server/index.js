9cf9296696013e4901386131ddd177a0374cbc7d
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-11 16:06:31 +0200

    [feature #164441516] Update the config file of the server

--- server/index.js
+++ server/index.js
@@ -2,36 +2,31 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
-import { config } from 'dotenv'; // Sets up dotenv as soon as our application starts
+import dotenv from 'dotenv';
 import { development } from './configs/configs.routes';
+import { routes } from './routes/index';
+
+dotenv.config();// Sets up dotenv as soon as our application starts
 
 // modules initialization
 const app = express();
 const router = express.Router();
+
 const environment = process.env.NODE_ENV; // development
-const stage = config;
-const PORT = process.env.PORT || 3000;
+const stage = development;
 
 // app uses initializations
 app.use(bodyParser.urlencoded({ extend: true }));
 app.use(bodyParser.json());
+app.use('/api/v1', routes(router));
 
 if (environment !== 'production') {
     app.use(logger('dev'));
 }
 
-app.use('/api/v1', (req, res, next) => {
-    res.send('Hello');
-    next();
-});
-
 if (stage.port) {
     app.listen(`${stage.port}`, () => {
         console.log(`Server now listening at localhost:${stage.port}`);
-    });
-} else {
-    app.listen(`${PORT}`, () => {
-        console.log(`Server now listening at localhost:${PORT}`);
     });
 }
 
