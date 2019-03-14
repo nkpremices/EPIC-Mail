ee58b0297897d8de13f664d0b6c7b0f32ec1389b
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-14 17:53:47 +0200

    [Feature #164441516] Give the sign in route to the users router

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,8 @@ import usersController from '../controllers/users';
 const goTo = (router) => {
     router.route('/auth/signup')
         .post(usersController.signup);
+    router.route('/auth/login')
+        .post(usersController.login);
 };
 
 export default goTo;
