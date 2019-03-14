d1b8374ac14919dbf089c98b09a34e2b1d07da07
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-14 15:51:45 +0200

    [Feature #164441516] Update the entry point od routes to create accounts

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,40 +1,8 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import usersRouter from './users';
 
-const app = express();
-const port = process.env.PORT || 3000;
+const routes = (router) => {
+    usersRouter(router);
+    return router;
+};
 
-app.use(bodyParser.urlencoded({ extend: true }));
-app.use(bodyParser.json());
-
-const todo = [
-    { id: 1, description: 'Clean the house', createdOn: '08/02/2019' },
-    { id: 2, description: 'wash the deshes', createdOn: '08/03/2019' },
-    { id: 3, description: 'Complete my Assignement', createdOn: '08/04/2019' },
-    { id: 4, description: 'Write some code', createdOn: '08/05/2019' },
-];
-
-app.post('/', (req, res) => {
-    console.log(req.body);// eslint-disable-line
-    const { id } = req.body;
-    const descp = req.body.description;
-    const creation = Date.now();// eslint-disable-line
-    const exist = todo.find(item => {// eslint-disable-line
-        return item.id === parseInt(id, 10);
-    });
-    res.send({
-        id, descp,
-    });
-});
-
-app.get('/', (req, res) => {
-    res.send({
-        todo, statusCode: 200,
-    });
-});
-
-app.listen(port, () => {
-    console.log('server running on 3000');// eslint-disable-line
-});
-
-export default app;
+export default routes;
