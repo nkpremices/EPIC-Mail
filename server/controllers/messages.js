2b75926308083bf8f0c6c709f9de008e949d1f91
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-15 08:33:54 +0200

    [Feature #164441516] Give a message controller to the server

---
+++ server/controllers/messages.js
@@ -0,0 +1,24 @@
+import { saveMessage } from '../models/messages';
+
+
+const messagesController = {
+    send: async (req, res) => {
+        const result = {};
+        const _status = 200;// eslint-disable-line
+
+        const {
+            token, subject, parentMessageId, status,
+        } = req.body;
+        try {
+            const tempMessage = await saveMessage(token, subject,
+                parentMessageId, status);
+            result.status = status;
+            result.data = [tempMessage];
+            res.status(_status).json(result);
+        } catch (error) {
+            res.status(500).json(`${error}`);
+        }
+    },
+};
+
+export default messagesController;
