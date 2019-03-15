abdd03b7703e680e564a7a19964a38577ad83f38
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-15 19:05:33 +0200

    [Feature #164441516] Add tests on users according to chai standrds

--- server/tests/users.js
+++ server/tests/users.js
@@ -5,11 +5,21 @@ import server from '../index';
 chai.use(chaiHttp);
 
 const should = chai.should(); // eslint-disable-line
+const user = {
+    userName: 'deschant',
+    password: 'password',
+    email: 'deschant@epic.com',
+};
+const userLog = {
+    userName: 'deschant',
+    password: 'password',
+};
 
 describe('signup', () => {// eslint-disable-line
     it('should return an object', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
             .post('/api/v1/auth/signup')
+            .send(user)
             .end((err, res) => {
                 res.body.should.be.an('Object');
                 done();
@@ -18,33 +28,73 @@ describe('signup', () => {// eslint-disable-line
     it('should have property data', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
             .post('/api/v1/auth/signup')
+            .send(user)
             .end((err, res) => {  // eslint-disable-line           
                 res.body.should.have.property('data')
-                    .which.is.an('object');
+                    .which.is.an('array');
                 done();
             });
     });
     it('should have property status', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
             .post('/api/v1/auth/signup')
+            .send(user)
             .end((err, res) => {  // eslint-disable-line           
                 res.body.should.have.property('status');
-                done();
-            });
-    });
-    it('the data should be an object', (done) => { // eslint-disable-line
-        chai.request(server)// eslint-disable-line
-            .post('/api/v1/auth/signup')
-            .end((err, res) => {  // eslint-disable-line           
-                res.body.data.should.be.an('Object');
                 done();
             });
     });
     it('the data should have proprety message on errors ', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
             .post('/api/v1/auth/signup')
+            .send(user)
             .end((err, res) => {  // eslint-disable-line           
-                res.body.data.should.have.property('message');
+                res.body.data[0].should.be.an('object')
+                    .which.have.property('token').which.is.a('String');
+                done();
+            });
+    });
+});
+
+describe('sign in', () => {// eslint-disable-line
+    it('should sign in only signed up users', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/signup')
+            .send(user)
+            .end((err, res) => {
+                res.body.should.be.an('Object')
+                    .with.property('data').which.is
+                    .an('array');
+                done();
+            });
+    });
+    it('should have property data', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/login')
+            .send(userLog)
+            .end((err, res) => {  // eslint-disable-line           
+                res.body.should.have.property('data')
+                    .which.is.an('array');
+                done();
+            });
+    });
+    it('should have property status', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/login')
+            .send(userLog)
+            .end((err, res) => {  // eslint-disable-line           
+                res.body.should.have.property('status')
+                    .which.is.an('number');
+                done();
+            });
+    });
+    it('the data should have proprety token ', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/login')
+            .send(userLog)
+            .end((err, res) => {  // eslint-disable-line           
+                res.body.data[0].should.be.an('object')
+                    .which.have.property('token').which.is.a('String');
                 done();
             });
     });
