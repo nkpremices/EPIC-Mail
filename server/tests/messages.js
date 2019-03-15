5c8e1a5e58f7dd40dbb5975e2da9762a3949b4d9
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-15 14:32:46 +0200

    [Feature #164441516] Add some tests for the messages

--- server/tests/messages.js
+++ server/tests/messages.js
@@ -7,35 +7,59 @@ chai.use(chaiHttp);
 const should = chai.should(); // eslint-disable-line
 
 describe('messages', () => {// eslint-disable-line
-    it('should return an object', (done) => { // eslint-disable-line
+    const sender = {
+        userName: 'deschant',
+        password: 'password',
+        email: 'deschant@epic.com',
+    };
+    const reciever = {
+        userName: 'premices',
+        password: 'password',
+        email: 'premices@epic.com',
+    };
+    let senderToken;
+    let recieverToken;
+    it('should have a signed user as a sender', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
-            .post('/api/v1/messages')
+            .post('/api/v1/auth/signup')
+            .send(sender)
+            .end((err, res) => {
+                senderToken = res.body.data[0].token;
+                res.body.should.be.an('Object');
+                done();
+            });
+    });
+    it('should have a signed user as a reciever', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/signup')
+            .send(reciever)
+            .end((err, res) => {
+                recieverToken = res.body.data[0].token;
+                res.body.should.be.an('Object');
+                done();
+            });
+    });
+    const sendMessageTemp = {
+        sender: senderToken,
+        reciever: recieverToken,
+        subject: 'subect',
+        parentMessageId: 0,
+        status: 'send',
+    };
+    it('the body should be an object on post', (done) => { // eslint-disable-line
+        chai.request(server)// eslint-disable-line
+            .post('/api/v1/auth/messages')
+            .send(sendMessageTemp)
             .end((err, res) => {
                 res.body.should.be.an('Object');
                 done();
             });
     });
-    it('should have property data', (done) => { // eslint-disable-line
+    it('the body should be an object on get', (done) => { // eslint-disable-line
         chai.request(server)// eslint-disable-line
-            .post('/api/v1/messages')
-            .end((err, res) => {  // eslint-disable-line           
-                res.body.should.have.property('data');
-                done();
-            });
-    });
-    it('should have property status', (done) => { // eslint-disable-line
-        chai.request(server)// eslint-disable-line
-            .post('/api/v1/messages')
-            .end((err, res) => {  // eslint-disable-line           
-                res.body.should.have.property('status');
-                done();
-            });
-    });
-    it('the data should be an object', (done) => { // eslint-disable-line
-        chai.request(server)// eslint-disable-line
-            .post('/api/v1/messages')
-            .end((err, res) => {  // eslint-disable-line           
-                res.body.data.should.be.an('Object');
+            .get('/api/v1/auth/messages')
+            .end((err, res) => {
+                res.body.should.be.an('Object');
                 done();
             });
     });
