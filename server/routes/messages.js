d503a5b362b31f8b1cb01e026da7b657908d6e64
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-15 10:19:47 +0200

    [Feature #164441516] Update the messages routes by adding a get

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -3,6 +3,8 @@ import messageController from '../controllers/messages';
 const goTo = (router) => {// eslint-disable-line
     router.route('/messages')
         .post(messageController.send);
+    router.route('/messages')
+        .post(messageController.send);
 };
 
 export default goTo;
