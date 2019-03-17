53e1a8e0d93c5a0201bd83112849a5904839dabd
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-17 09:34:17 +0200

    [Feature #164681690] Give unread messages route to the server

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -3,6 +3,8 @@ import messageController from '../controllers/messages';
 const goTo = (router) => {// eslint-disable-line
     router.route('/messages')
         .post(messageController.send);
+    router.route('/messages/unread')
+        .get(messageController.fetchAllUnread);
     router.route('/messages')
         .get(messageController.fetchAll);
 };
