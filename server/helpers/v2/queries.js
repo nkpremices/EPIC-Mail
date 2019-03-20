984cddedac2400402cdec71a86be6014c07de226
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-20 16:03:03 +0200

    [Feature

--- server/helpers/v2/queries.js
+++ server/helpers/v2/queries.js
@@ -1,9 +1,9 @@
 const findAll = table => `SELECT * FROM ${table}`;
 const findOne = (table, column,
     value) => `SELECT * FROM ${table} WHERE ${column} = ${value}`;
-const insertUser = () => `INSERT INTO users(id, firstName,
-        lastName, password, email, isAdmin) 
-        VALUES($1, $2, $3, $4, $5, $6) returning *`;
+const insertUser = `INSERT INTO users (firstName, lastName,
+userName, email, password, isAdmin)
+VALUES($1, $2, $3, $4, $5, $6) returning *`;
 export {
     findAll,
     findOne,
