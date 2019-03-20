20ca9dc0fbbe7e615e15c0bd6476362df3de72ed
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-20 15:57:44 +0200

    [Feature

--- server/models/v2/users.js
+++ server/models/v2/users.js
@@ -1,47 +1,42 @@
 import bcrypt from 'bcrypt';
 import configs from '../../config/config';
+import { querryDb } from '../../../db';
+import {
+    findAll,
+    insertUser,
+} from '../../helpers/v2/queries';
 
 'use strict';// eslint-disable-line 
 
 // a schema of all the users
-class UsersSchema {
-    constructor(id,
-        email, userName,
-        firstName, lastName,
-        password, isNew) {
-        this.id = id;
-        this.email = email;
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.isNew = isNew;
-    }
-}
 
 // a data structure for storing all the users
 
-const usersStorage = [];
-let id = 0;
-
+const usersStorage = async () => querryDb.query(findAll('users'));
 // a function to save a user when requested
-const saveUser = (email, userName, firstName,
-    lastName, password) => new Promise((resolve, reject) => {
+const saveUser = (firstName, lastName,
+    userName, email, password) => new Promise((resolve, reject) => {
     // creating a temp user
     // eslint-disable-next-line no-unused-expressions
-    const tempUser = new UsersSchema(id += 1,
-        email, userName, firstName, lastName, password, true);
+    const tempUser = [firstName, lastName,
+        userName, email, password, true];
 
     // hashing password before storing
     bcrypt.hash(password, configs.development.saltingRounds,
-        (err, hash) => {
+        async (err, hash) => {
             if (err) reject(err);
             else {
-                tempUser.password = hash;
-                tempUser.isNew = false;
-                usersStorage.push(tempUser);
-                console.log(usersStorage);
-                resolve(usersStorage[usersStorage.length - 1]);
+                tempUser[4] = hash;
+                tempUser[5] = false;
+
+                try {
+                    console.log(1);
+                    const { rows } = await querryDb.query(insertUser, tempUser);
+                    console.log(rows[0]);
+                    resolve(rows[0]);
+                } catch (error) {
+                    reject(error);
+                }
             }
         });
 });
