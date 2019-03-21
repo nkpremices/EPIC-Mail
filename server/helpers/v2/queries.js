203ce32e8b54bae05969e36659d5bdb303e2f51b
Prémices NZANZU KAMASUWA <premices.tuvere@gmail.com>
2019-03-21 11:49:03 +0200

    [Feature #164689087] Add querries for messages in the queries helper

--- server/helpers/v2/queries.js
+++ server/helpers/v2/queries.js
@@ -1,11 +1,29 @@
 const findAll = table => `SELECT * FROM ${table}`;
-const findOne = (table, column,
-    value) => `SELECT * FROM ${table} WHERE ${column} = ${value}`;
+
+const findUserByName = (name) => `SELECT * FROM users WHERE username = \'${name}\'`;// eslint-disable-line
+const findUserByEmail = (email) => `SELECT * FROM users WHERE email = \'${email}\' `;// eslint-disable-line
+const findUnreadMessages = `SELECT * FROM messages WHERE isread = false`;// eslint-disable-line
+const findSentMessages = `SELECT * FROM messages WHERE status = \'sent\'`;// eslint-disable-line
+const findMessageById = (id) => `SELECT * FROM messages WHERE id = ${id}`;// eslint-disable-line
+const deleteMessageById = (id) => `DELETE FROM messages WHERE id = ${id}`;// eslint-disable-line
+const setUserLogedIn = id => `UPDATE users SET islogedin = true WHERE id = ${id};`;// eslint-disable-line
 const insertUser = `INSERT INTO users (firstName, lastName,
 userName, email, password, isAdmin)
 VALUES($1, $2, $3, $4, $5, $6) returning *`;
+
+const insertMessage = `INSERT INTO messages (senderid, receiverid,
+    parentmessageid, subject, text, status, created_at)
+    VALUES($1, $2, $3, $4, $5, $6, $7) returning *`;
+
 export {
     findAll,
-    findOne,
+    findUserByName,
+    findUserByEmail,
     insertUser,
+    insertMessage,
+    findUnreadMessages,
+    findSentMessages,
+    findMessageById,
+    deleteMessageById,
+    setUserLogedIn,
 };
